6aefbec863d66d55a802870717b07b3fca74b7bf
Percy Val Stuwath Cornelio Garcia <pcornelio>
2024-05-03 11:41:45 -0500

    Creación del api

---
+++ mappark.api/Controllers/MachineLearningController.cs
@@ -0,0 +1,62 @@
+﻿using mappark.api.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace mappark.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineLearningController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<string> ProbabilidadEstacionamiento([FromBody] RequestProbabilidadEstacionamiento request)
+        {
+            var client = new HttpClient();
+            var httpRequest = new HttpRequestMessage(HttpMethod.Post, "https://az-ml-atmira-reboots-conn-point.westeurope.inference.ml.azure.com/score");
+            httpRequest.Headers.Add("Authorization", "Bearer qfSc0byLka3RB5a3LJUNNyhwGYuTErqs");
+
+            var requestWrapper = new RequestWrapper
+            {
+                InputData = new InputData
+                {
+                    Data = new List<RequestProbabilidadEstacionamiento> { request }
+                }
+            };
+
+            string json = JsonSerializer.Serialize(requestWrapper);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            httpRequest.Content = content;
+            var response = await client.SendAsync(httpRequest);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+
+            var probabilities = JsonSerializer.Deserialize<List<double>>(result);
+            if (probabilities == null || probabilities.Count == 0)
+            {
+                return "No se ha podido obtener la probabilidad";
+            }
+
+            double probability = probabilities[0];
+            string category;
+
+            if (probability < 0.50)
+            {
+                category = "Baja";
+            }
+            else if (probability <= 0.85)
+            {
+                category = "Media";
+            }
+            else
+            {
+                category = "Alta";
+            }
+
+            return category;
+        }
+       
+    }
+}
---
+++ mappark.api/Models/RequestProbabilidadEstacionamiento.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.Json.Serialization;
+
+namespace mappark.api.Models
+{
+
+    public class RequestProbabilidadEstacionamiento
+    {
+        [JsonPropertyName("idProbabilidadEstacionamiento")]
+        public int IdProbabilidadEstacionamiento { get; set; }
+
+        [JsonPropertyName("latitud")]
+        public double Latitud { get; set; }
+
+        [JsonPropertyName("longitud")]
+        public double Longitud { get; set; }
+
+        [JsonPropertyName("pais")]
+        public string Pais { get; set; }
+
+        [JsonPropertyName("ciudad")]
+        public string Ciudad { get; set; }
+
+        [JsonPropertyName("provincia")]
+        public string Provincia { get; set; }
+
+        [JsonPropertyName("barrio")]
+        public string Barrio { get; set; }
+
+        [JsonPropertyName("nombreVia")]
+        public string NombreVia { get; set; }
+
+        [JsonPropertyName("codigoPostal")]
+        public int CodigoPostal { get; set; }
+
+        [JsonPropertyName("fechaCompleta")]
+        public DateTime FechaCompleta { get; set; }
+
+        [JsonPropertyName("año")]
+        public int Año { get; set; }
+
+        [JsonPropertyName("nombreDiaSemana")]
+        public string NombreDiaSemana { get; set; }
+
+        [JsonPropertyName("nombreMes")]
+        public string NombreMes { get; set; }
+
+        [JsonPropertyName("horaInicio")]
+        public string HoraInicio { get; set; }
+
+        [JsonPropertyName("horaFin")]
+        public string HoraFin { get; set; }
+
+        [JsonPropertyName("temperatura")]
+        public double Temperatura { get; set; }
+
+        [JsonPropertyName("precipitacion")]
+        public double Precipitacion { get; set; }
+
+        [JsonPropertyName("densidadTrafico")]
+        public double? DensidadTrafico { get; set; }
+    }
+
+    public class RequestWrapper
+    {
+        [JsonPropertyName("input_data")]
+        public InputData InputData { get; set; }
+    }
+
+    public class InputData
+    {
+        [JsonPropertyName("data")]
+        public List<RequestProbabilidadEstacionamiento> Data { get; set; }
+    }
+}
